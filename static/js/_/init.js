69123579b51bf027650b67203d278ab1bdd9244a
Barret Schloerke <schloerke@gmail.com>
2011-06-25 13:30:34 -0700

    fixed map issue

--- static/js/_/init.js
+++ static/js/_/init.js
@@ -1,12 +1,13 @@
 (function() {
-  var chart, path, xy;
-  console.log('eur');
-  xy = d3.geo.mercator().scale(1200);
-  chart = d3.select("body").append("svg:svg");
-  path = d3.geo.path().projection(xy);
-  d3.json("/map", function(collection) {
-    return chart.selectAll("path").data(collection.features).enter().append("svg:path").attr("d", path).append("svg:title").text(function(d) {
-      return d.properties.name;
+  $(function() {
+    var chart, path, xy;
+    xy = d3.geo.mercator().scale(1200);
+    chart = d3.select("#canvas").append("svg:svg");
+    path = d3.geo.path().projection(xy);
+    return d3.json("/map", function(collection) {
+      return chart.selectAll("path").data(collection.features).enter().append("svg:path").attr("d", path).append("svg:title").text(function(d) {
+        return d.properties.name;
+      });
     });
   });
 }).call(this);
